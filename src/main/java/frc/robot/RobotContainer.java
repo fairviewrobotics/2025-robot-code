250390b92af52b3952f80f0d9dccbb70847ee4b4
semblanceofsense <semblanceofsensegaming@gmail.com>
2025-01-20 19:24:50 -0700

    PickAndPlaceCommands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commands.*;
+import frc.robot.commands.PickAndPlaceCommands.Position;
 import frc.robot.constants.*;
 import frc.robot.subsystems.*;
 import frc.robot.utils.Controller;
@@ -13,6 +14,8 @@ import frc.robot.utils.NetworkTablesUtils;
 public class RobotContainer {
     // Subsystems
     SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+    ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
+    ArmSubsystem armSubsystem = new ArmSubsystem();
 
     // Controllers
     Controller primaryController = new Controller(0);
@@ -48,6 +51,10 @@ public class RobotContainer {
                                         / 1.0,
                         true,
                         true));
+
+        elevatorSubsystem.setDefaultCommand(
+            new PickAndPlaceCommands(elevatorSubsystem, armSubsystem, Position.INTAKE)
+        );
     }
 
     public Command getAutonomousCommand() {
