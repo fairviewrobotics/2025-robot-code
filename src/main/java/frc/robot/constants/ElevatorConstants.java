623ca7633a095e97a4146c6c478d942f05baad61
semblanceofsense <semblanceofsensegaming@gmail.com>
2025-01-25 12:47:19 -0700

    bs the values
    more rebasing

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -8,12 +8,12 @@ public class ElevatorConstants {
     public static final int LEFT_MOTOR_ID = 0;
     public static final int RIGHT_MOTOR_ID = 0;
 
-    public static final double ELEVATOR_P = 0;
+    public static final double ELEVATOR_P = 0.5;
     public static final double ELEVATOR_I = 0;
     public static final double ELEVATOR_D = 0;
-    public static final double ELEVATOR_TOLERANCE = 0;
+    public static final double ELEVATOR_TOLERANCE = 1.0;
 
-    public static final double ELEVATOR_KS = 0;
+    public static final double ELEVATOR_KS = 0.1;
     public static final double ELEVATOR_KV = 0;
     public static final double ELEVATOR_KG = 0;
     public static final double ELEVATOR_KA = 0;
@@ -24,7 +24,7 @@ public class ElevatorConstants {
     public static final int TOP_LINEBREAK_ID = 0;
     public static final int BOTTOM_LINEBREAK_ID = 0;
 
-    public static final double ELEVATOR_ZEROING_VOLTAGE = 0;
+    public static final double ELEVATOR_ZEROING_VOLTAGE = 2;
 
     public static final double ROTATIONS_TO_METERS = 1;
 
@@ -33,7 +33,7 @@ public class ElevatorConstants {
     public static final double HARD_STOP_LEVEL = -0.1;
 
     public static final double INTAKE_HEIGHT = 0;
-    public static final double L1_HEIGHT = 0;
+    public static final double L1_HEIGHT = 1;
     public static final double L2_HEIGHT = 0;
     public static final double L3_HEIGHT = 0;
     public static final double L4_HEIGHT = 0;
